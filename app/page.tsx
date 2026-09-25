34a40bb6ce7404875288585432e3899440c4ea21
janreyhablamina <janreyhablamina.ifliba@gmail.com>
2026-09-25 21:22:47 +0800

    feat: redesign footer with luxury automotive dispatch and 4-column directory (feb 20, 2026 9:22pm)

--- app/page.tsx
+++ app/page.tsx
@@ -15,6 +15,7 @@ import "./home-sections.css";
 import TestimonialsSection from "@/components/home/TestimonialsSection";
 import CockpitCounter from "@/components/home/CockpitCounter";
 import HomeContactSection from "@/components/home/HomeContactSection";
+import HomeFooter from "@/components/home/HomeFooter";
 
 const CarScene = dynamic(() => import("@/components/three/CarScene"), { ssr: false });
 
@@ -198,7 +199,8 @@ export default function Home() {
       {/* Map Background VIP Concierge & Contact Section */}
       <HomeContactSection />
  
-      <footer id="footer"><div className="footer-main"><div><div className="wordmark">DRIVE<span>X</span></div><p>Drive more. Experience more.</p></div><div><h4>Explore</h4><a href="#vehicles">Vehicles</a><a href="#how">How It Works</a><a href="#why">About</a><a href="mailto:hello@drivex.ph">Contact</a></div><div><h4>Support</h4><a href="#footer">Help Center</a><a href="#footer">Rental Policies</a><a href="#footer">Terms</a><a href="#footer">Privacy</a></div><div><h4>Follow</h4><a href="#footer">Instagram</a><a href="#footer">Facebook</a><a href="#footer">LinkedIn</a></div></div><div className="footer-bottom"><span>© 2026 DriveX. All rights reserved.</span><span>Manila, Philippines</span></div></footer>
+      {/* Redesigned Luxury Automotive Footer */}
+      <HomeFooter />
     </main>
   );
 }
